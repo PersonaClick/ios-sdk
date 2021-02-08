79a56466d9cc38b30896ab5c67c397f6e1655c27
Michael Kechinov <mkechinov@Michaels-MacBook-Pro.local>
2021-02-08 10:05:36 +0300

    1.4.1 extended params in suggest

--- PersonaClick/Classes/SimplePersonalizationSDK.swift
+++ PersonaClick/Classes/SimplePersonalizationSDK.swift
@@ -445,7 +445,7 @@ class SimplePersonalizationSDK: PersonalizationSDK {
     }
 
 
-    func suggest(query: String, locations: String?, timeOut: Double?, completion: @escaping (Result<SearchResponse, SDKError>) -> Void) {
+    func suggest(query: String, locations: String?, timeOut: Double?, extended: String?, completion: @escaping (Result<SearchResponse, SDKError>) -> Void) {
 
         mySerialQueue.async {
             let path = "search"
@@ -460,6 +460,9 @@ class SimplePersonalizationSDK: PersonalizationSDK {
 
             if let locations = locations{
                 params["locations"] = locations
+            }
+            if let extended = extended{
+                params["extended"] = extended
             }
             
             let sessionConfig = URLSessionConfiguration.default
