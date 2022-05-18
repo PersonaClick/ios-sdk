b9198e61d49010529048d3c80246b06becf72a10
Michael Kechinov <kechinoff@gmail.com>
2022-05-18 14:47:16 +0300

    Category parent ID in search response

--- PersonaClick.podspec
+++ PersonaClick.podspec
@@ -1,7 +1,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'PersonaClick'
-  s.version          = '2.0.0'
+  s.version          = '2.0.1'
   s.summary          = 'PersonaClick iOS SDK'
 
 # This description is used to generate tags and improve search results.
--- PersonaClick/Classes/Model/SearchResponse.swift
+++ PersonaClick/Classes/Model/SearchResponse.swift
@@ -191,11 +191,13 @@ public struct Category {
     public var name: String = ""
     public var url: String?
     public var alias: String?
+    public var parentId: String?
 
     init(json: [String: Any]) {
         id = json["id"] as? String ?? ""
         name = json["name"] as? String ?? ""
         url = json["url"] as? String
         alias = json["alias"] as? String
+        parentId = json["parent"] as? String
     }
 }
